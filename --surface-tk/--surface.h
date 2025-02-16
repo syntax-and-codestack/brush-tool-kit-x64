436e18437ed0254e966655a85a8fcbaa78ebc3d6
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-02-16 22:55:15 +0000

    --surface_flags >> 27

---
+++ --surface-tk/--surface.h
@@ -0,0 +1,14 @@
+//--surface.h programmed by hunter manko
+#ifndef SURFACE_H
+#define SURFACE_H
+
+#define SURFACE_FLAGS --SURFACE_FLAGS 0x0
+
+int Flags;
+double SurfaceFlags = 00000000.33333333;
+
+struct Surface_Flags{
+    Surface_Flags * Flagger;
+};
+
+#endif
